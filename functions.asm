71db26ee25af46612d18d65ffde30b04e6f19fe2
Drlinx <Dragonlinkx@gmail.com>
2023-10-09 13:26:29 -0600

    got first printf working now onto the next

--- functions.asm
+++ functions.asm
@@ -4,10 +4,9 @@
 
 extern printf           ; Declares printf for use later
 section .data
-    radius dq 10
+    radius dq 10.0
     pi dq 3.14159
-
-    fmt_raduis db "the circle has a radius of %d", 10, 0
+    two dq 2.0
 section .bss
 
 section .text
@@ -16,8 +15,18 @@ section .text
 
 main:
 
+    ; mov rdi, tes ; first argument for printf
+    ; mov rsi, 50 ; second argument for printf
+    ; mov rax, 0 ; no xmm registers involved
+
+    ; call printf ; call the function
+
     call circle
 
+    mov rsp, rbp
+    pop rbp
+    ret 
+    
     mov rax, 60     ; Exit
     mov rdi, 0      ; Exit status
     syscall         ; Calls the above 2.
@@ -27,53 +36,49 @@ area:
     section .data
     .fmt_area db "The area is %f", 10, 0 ; string we will be printing
     section .text
-    push rbp            ; Initiates the printf call.
-    mov rbp, rsp
-    mov rdi, .fmt_area        ; The string.
-    mov rsi, rax        ; The result of .
-    mov rax, 0          ; Print out to the terminal.
 
-    call printf         ; Calls printf.
-    mov rsp, rbp
-    pop rbp
-    ret             ; Cleans up the printf call.
+
 
     ret ; breaks the function
 
 circum:
+    ; enter
     section .data
-    fmt_circum db "the circumference is %f", 20, 0
+        .fmt_circum db "the circumference is %f %f", 10, 0 ; string we will be printing
     section .text
-    push rbp            ; Initiates the printf call.
-    mov rbp, rsp
-    mov rdi, fmt_circum        ; The string.
-    mov rsi, [radius]        ; The radius.
-    mov rax, 0          ; Print out to the terminal.
 
-    call printf         ; Calls printf.
-    mov rsp, rbp
-    pop rbp
-    ret             ; Cleans up the printf call.
+    movsd xmm0, [radius]
+    movsd xmm1, [two]
+    mulsd xmm0, xmm1
+    mulsd xmm0, [pi]
 
+    mov rax, 2 ; 1 xmm registers involved
+
+    mov rdi, .fmt_circum ; first argument for printf   
+    call printf ; call the function
     ret
 
 
-circle:
+circle:  
     section .data
-        ; fmt_raduis db "the circle has a radius of %d", 10, 0
+        fmt_raduis db "the circle has a radius of %f", 10, 0
     section .text
 
-    mov rax, [radius]
+    ; mov rax, [radius]
 
-    push rbp
-    mov rbp, rsp
-    mov rdi, fmt_raduis ; first argument for printf
-    mov rsi, 10 ; second argument for printf
-    mov rax, 0 ; no xmm registers involved
-    call printf ; call the function
-    mov rsp, rbp
-    pop rbp
-    ret             ; Cleans up the printf call
+    movsd xmm0, [radius]; prepares the xmm register
 
+    ; mov rax, 1 ; no xmm registers involved
+    ; mov rdi, fmt_raduis ; first argument for printf   
+    ; call printf ; call the function
+
+    ; mov rsp, rbp
+    ; pop rbp
+    ; ret             ; Cleans up the printf call
+
+    call circum
+
+    ; call area
+    ; leave
     ret             ; Cleans up the printf call.
 
